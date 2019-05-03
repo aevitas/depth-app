0dbf2db06c3781b0c861aedc7d47a38acc94be3b
aevitas <m4rtijn@gmail.com>
2019-05-03 22:40:11 +0200

    Add movie poster to detail

--- src/Components/MovieDetail.js
+++ src/Components/MovieDetail.js
@@ -6,9 +6,12 @@ class MovieDetail extends React.Component {
 
     if (movie == null) return <div />;
 
+    const url = `https://image.tmdb.org/t/p/w1280/${movie.poster_path}`;
+
     return (
       <>
         <h1>{movie.original_title}</h1>
+        <img src={url} />
       </>
     );
   }
