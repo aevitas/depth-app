7537c9851f3cfd421a209991e38af96a39ccc0c6
aevitas <m4rtijn@gmail.com>
2019-05-06 15:13:56 +0200

    Add some styling

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
-import React from 'react';
-import Search from './Components/Search'
-import ResultList from './Components/ResultList'
-import MovieDetail from './Components/MovieDetail'
+import React from "react";
+import Search from "./Components/Search";
+import ResultList from "./Components/ResultList";
+import MovieDetail from "./Components/MovieDetail";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       searchResults: [],
       selectedMovie: null
-    }
+    };
   }
 
-  onSearchCompleted = (results) => {
-    this.setState({searchResults: results, selectedMovie: null});
-  }
+  onSearchCompleted = results => {
+    this.setState({ searchResults: results, selectedMovie: null });
+  };
 
-  onMovieSelected = async (id) => {
-
-    if (!id)
-      return;
+  onMovieSelected = async id => {
+    if (!id) return;
 
     const detailUri = `https://depth-api.blackrain.io/movies/${id}`;
     const response = await axios.get(detailUri);
 
-    this.setState({searchResults: [], selectedMovie: response.data.movie, currentTrailer: response.data.trailer});
-  }
+    this.setState({
+      searchResults: [],
+      selectedMovie: response.data.movie,
+      currentTrailer: response.data.trailer
+    });
+  };
 
   render() {
     return (
       <div className="wrapper">
-      <h1>Depth</h1>
-      <div>
-        <Search SearchCompleted={(results) => this.onSearchCompleted(results)} />
-        <MovieDetail Movie={this.state.selectedMovie} Trailer={this.state.currentTrailer} />
-        <ResultList Items={this.state.searchResults} SelectMovie={(id) => this.onMovieSelected(id)} />
+      <div className="container">
+          <h1>Depth</h1>
+          <Search
+            SearchCompleted={results => this.onSearchCompleted(results)}
+          />
+          <MovieDetail
+            Movie={this.state.selectedMovie}
+            Trailer={this.state.currentTrailer}
+          />
+          <ResultList
+            Items={this.state.searchResults}
+            SelectMovie={id => this.onMovieSelected(id)}
+          />
+        </div>
       </div>
-      </div>
-    )
+    );
   }
 }
 
--- src/Components/MovieDetail.js
+++ src/Components/MovieDetail.js
@@ -12,11 +12,37 @@ class MovieDetail extends React.Component {
 
     return (
       <>
-        <h1>{movie.original_title}</h1>
-        <img alt="Movie Poster" src={posterUri} style={{width:300}} />
-        <p>{movie.overview}</p>
+        <div className="row">
+        <div className="col-md-12">
+          <h1>{movie.original_title}</h1>
+        </div>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <img alt="Movie Poster" src={posterUri} style={{ width: 350 }} />
+          </div>
+          <div className="col-md-6">
+          <h2>Description</h2>
+          <p>{movie.overview}</p>
 
-        <iframe title="Trailer" width="560" height="315" src={embedUri} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <h2>Details</h2>
+          <p><b>Release Date</b>: {movie.release_date}</p>
+            <p><b>Rating: </b> {movie.vote_average}</p>
+            <p><b>Language: </b> {movie.original_language}</p>
+          </div>
+        </div>
+
+<h1>Trailer</h1>
+
+        <iframe
+          title="Trailer"
+          width="800"
+          height="500"
+          src={embedUri}
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
       </>
     );
   }
